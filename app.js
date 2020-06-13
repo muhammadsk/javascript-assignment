a236312dba31f5cfdd065cb266acb3cdb3ff1ef3
Muhammad Shehroz <65031022+muhammadsk@users.noreply.github.com>
2020-06-13 11:14:39 -0700

    javascript assignment chapter 1 to 20

---
+++ app.js
@@ -0,0 +1,580 @@
+
+
+// Chaper 1 Q 1
+// var thanx = "greet your website"
+//  alert(thanx);
+
+// Chaper ==== 1 === Question ==== 2
+
+//  var Error = "Error! please enter a valid password."
+//  alert(Error);
+
+
+// Chaper ==== 1 === Question ==== 3
+
+
+// alert('Welcome to js Land..\n Happy Coding!')
+
+
+// Chaper ==== 1 === Question ==== 4
+
+// var a = "Weclome to JS Land..."
+// alert(a)
+// var b = "Happy Coding! \n Prevent this page from creating additional dialogs."
+// alert(b)
+
+// Chaper ==== 1 === Question ==== 5
+
+// var d = "Hello... I can run JS through my web browser's console"
+// alert(d)
+// console.log(d)
+
+// Chaper ==== 2 === Question ==== 1
+
+// var username = "username"
+// console.log(username)
+
+// Chaper ==== 2 === Question ==== 2
+
+// var myName = "Muhammad Shehroz"
+// alert(myName)
+
+// Chaper ==== 2 === Question ==== 3
+// var message = "Hello World"
+// alert(message)
+
+
+// Chaper ==== 2 === Question ==== 4
+
+// var studentName = "Jhone Doe"
+// alert(studentName)
+// var age ="15 years old"
+// alert(age)
+// var grade = "Certified Mobile Application Development"
+// alert(grade)
+
+// Chaper ==== 2 === Question ==== 5
+
+// var pizza = "PIZZA \n PAZZ \n PIZ \n PI \n P"
+// alert(pizza)
+
+
+
+// Chaper ==== 2 === Question ==== 6
+
+
+// var a = "My email address is"
+// var email = "shehroz.sidd030303@gmail.com"
+// alert(a+" "+email)
+
+// Chaper ==== 2 === Question ==== 7
+
+
+// var book = "i am trying to learn from the Book A Smater Way to learn JaveScript"
+// alert(book)
+
+// Chaper ==== 2 === Question ==== 8
+
+
+// var Yah = "Yah! I can write HTML content through JavaScript"
+// document.write(Yah)
+
+// Chaper ==== 2 === Question ==== 9
+
+
+// var des = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert(des)
+
+
+// Chapter 3
+
+// var age = "i am 15 years old"
+// alert(age)
+
+
+
+
+// var birthYear ="My Birth Year is 1990 </br> Data type of my ddeclared variable is number"
+// document.write(birthYear)
+
+
+// var clothStore = "<b>Muhammad Shehroz</b> orderd <b>5 T-shirt</b>(s) on Shahid Store"
+// document.write(clothStore)
+
+
+
+// chapter 4
+
+
+// var hey = "hy" , hello = "hello" , how = "how are you"
+// console.log(hey,hello,how)
+
+// var lone = " 1. _lastName \n 2. _100 \n 3. $myMoney \n 4. x \n 5. $myMoney "
+// console.log(lone)
+
+// var lone = " 1. 9 \n 2. 2student \n 3. .student \n 4. She rhoz \n 5. sher.hoz "
+// console.log(lone)
+
+
+// Chapter 5
+
+// var a = "sum of 3 and 5 is"
+// var b = (3 + 5);
+// document.write(a + " " + b)
+
+// var a = "sum of 3 and 5 is"
+// var b = (5 - 3);
+// document.write(a + " " + b)
+
+
+// var a = "sum of 3 and 5 is"
+// var b = (3 * 5);
+// document.write(a + " " + b)
+
+// var a = "sum of 3 and 5 is"
+// var b = (3 / 5);
+// document.write(a + " " + b)
+
+
+// var a = "value after variable declaration is undefined </br>"
+// document.write(a)
+
+// var b = "initial value:"
+// var c = 5;
+// document.write(b + c)
+
+// var d = " </br> value after increment is:"
+// var e = ++c;
+// document.write(d + e)
+
+// var f = "</br> value after addition is:"
+// var g = e + 7
+// document.write(f + g)
+
+// var h = "</br> value after decrement is:"
+// var i = --g
+// document.write(h + i)
+
+// var j = "</br> The remainder is:"
+// var k = i % 3
+// document.write(j + k)
+
+
+// var a = "Total Cost to buy"
+// var b = 5;
+// var c = "tickets to a movies is"
+// var d = 5*600;
+// document.write(a,b,c,d+" "+"PKR")
+
+// var Table = "Table of 5 </br>"
+// var a = 5 + "x" + 1 + "=" + 1 * 5 + "</br>";
+// var b = 5 + "x" + 2 + "=" + 2 * 5 + "</br>";
+// var c = 5 + "x" + 3 + "=" + 3 * 5 + "</br>";
+// var d = 5 + "x" + 4 + "=" + 4 * 5 + "</br>";
+// var e = 5 + "x" + 5 + "=" + 5 * 5 + "</br>";
+// var f = 5 + "x" + 6 + "=" + 6 * 5 + "</br>";
+// var g = 5 + "x" + 7 + "=" + 7 * 5 + "</br>";
+// var h = 5 + "x" + 8 + "=" + 8 * 5 + "</br>";
+// var i = 5 + "x" + 9 + "=" + 9 * 5 + "</br>";
+// var j = 5 + "x" + 10 + "=" + 10 * 5 + "</br>";
+// document.write(Table,a, b, c, d, e, f, g, h, i, j)
+
+
+// function cToF(celsius){
+//     var CeTemp = celsius;
+//     var celToFahr = CeTemp * 9/5 +32;
+//     var mess = CeTemp+'\xB0C is' + celToFahr + '\xB0F.'
+//     console.log(mess)
+// }
+// function fToC(fahrenheit){
+//     var faTemp = fahrenheit;
+//     var faToCel = (faTemp - 32 ) * 5/9;
+//     var mess = faTemp+'\xB0F is' + faToCel + '\xB0C.'
+//     console.log(mess);
+// }
+// cToF(25);
+// fToC(70);
+
+
+// var head = "<b> Shopping Cart </b>"
+// document.write(head+"<br>")
+
+// var a = "Price of item 1 is"
+// var b = 650;
+// document.write(a, b)
+
+// var c = "Quantity of item 1 is "
+// var d = 3;
+// document.write("<br>" + c, d)
+
+// var e = "Price of item 2 is"
+// var f = 100;
+// document.write("<br>" + e, f)
+
+// var g = "Quantity of item 2 is"
+// var h = 7;
+// document.write("<br>" + g, h)
+
+// var i = "shipping Charges"
+// var j = 100;
+// document.write("<br>" + i, j)
+
+// var k = "Total cost of your order is"
+// var l = (b * d) + (f * h) + 100;
+// document.write("<br>" + k + " " + l)
+
+
+
+
+// var head = "<b> Marks Sheet </b>"
+// document.write(head + "<br>")
+// var a = "Total marks:"
+// var b = 980;
+// document.write(a + b)
+// var c = "Marks obraines:"
+// var e = 804;
+// document.write("<br>" + c + e)
+// var f = "Percentage:"
+// var g = 804 * 980 / 100;
+// document.write("<br>" + f, g)
+
+
+// var head = "<b> Currency in PKR </b>"
+// document.write(head + "<br>")
+// var a = "Total Currency in PKR:"
+// var expra =  1048+700;
+// document.write(a,expra)
+
+// var a = 5*10/2;
+// document.write(a)
+
+// var head = "<b>Age Calculator</b>"
+// document.write("<br>"+head)
+// var a = prompt("Enter Current Year")
+// var b = prompt("Enter your birth year")
+// var c = "Your age is "
+// alert(a-b)
+
+// var head = "<b>Age Calculator</b>"
+// document.write("<br>"+head)
+
+// var a = "Radius of a circle:"
+// var radius = 20;
+// document.write("<br>"+a,radius)
+
+// var b = "The circumference is:"
+// var circum = 2*3.142*20;
+// document.write("<br>"+b+circum)
+
+// var c = "The area is:"
+// var area = 3.142*40;
+// document.write("<br>"+b+circum)
+
+
+// var a = "Facourite Snack: chocolate chip"
+// document.write(a)
+// var b = "Current age:"
+// var age = 15
+// document.write("<br>" + b+ age)
+// var c = "Estimated Maximum Age:"
+// var d = 65;
+// document.write("<br>"+c+d)
+// var e = "Amount of snacks per day:"
+// var f = 3;
+// document.write("<br>"+e+f)
+// var g = "you will need "
+// document.write("<br>",g,(65-15)*3)
+// document.write(" "+"chocolate chip to last you unit the rip old age of 65")
+
+
+
+
+
+
+
+
+// Chaper 6to9 Q 1
+// var thanx = "greet your website"
+//  alert(thanx);
+
+// Chaper ==== 1 === Question ==== 2
+
+//  var Error = "Error! please enter a valid password."
+//  alert(Error);
+
+
+// Chaper ==== 1 === Question ==== 3
+
+
+// alert('Welcome to js Land..\n Happy Coding!')
+
+
+// Chaper ==== 1 === Question ==== 4
+
+// var a = "Weclome to JS Land..."
+// alert(a)
+// var b = "Happy Coding! \n Prevent this page from creating additional dialogs."
+// alert(b)
+
+// Chaper ==== 1 === Question ==== 5
+
+// var d = "Hello... I can run JS through my web browser's console"
+// alert(d)
+// console.log(d)
+
+// Chaper ==== 2 === Question ==== 1
+
+// var username = "username"
+// console.log(username)
+
+// Chaper ==== 2 === Question ==== 2
+
+// var myName = "Muhammad Shehroz"
+// alert(myName)
+
+// Chaper ==== 2 === Question ==== 3
+// var message = "Hello World"
+// alert(message)
+
+
+// Chaper ==== 2 === Question ==== 4
+
+// var studentName = "Jhone Doe"
+// alert(studentName)
+// var age ="15 years old"
+// alert(age)
+// var grade = "Certified Mobile Application Development"
+// alert(grade)
+
+// Chaper ==== 2 === Question ==== 5
+
+// var pizza = "PIZZA \n PAZZ \n PIZ \n PI \n P"
+// alert(pizza)
+
+
+
+// Chaper ==== 2 === Question ==== 6
+
+
+// var a = "My email address is"
+// var email = "shehroz.sidd030303@gmail.com"
+// alert(a+" "+email)
+
+// Chaper ==== 2 === Question ==== 7
+
+
+// var book = "i am trying to learn from the Book A Smater Way to learn JaveScript"
+// alert(book)
+
+// Chaper ==== 2 === Question ==== 8
+
+
+// var Yah = "Yah! I can write HTML content through JavaScript"
+// document.write(Yah)
+
+// Chaper ==== 2 === Question ==== 9
+
+
+// var des = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert(des)
+
+
+// Chapter 3
+
+// var age = "i am 15 years old"
+// alert(age)
+
+
+
+
+// var birthYear ="My Birth Year is 1990 </br> Data type of my ddeclared variable is number"
+// document.write(birthYear)
+
+
+// var clothStore = "<b>Muhammad Shehroz</b> orderd <b>5 T-shirt</b>(s) on Shahid Store"
+// document.write(clothStore)
+
+
+
+// chapter 4
+
+
+// var hey = "hy" , hello = "hello" , how = "how are you"
+// console.log(hey,hello,how)
+
+// var lone = " 1. _lastName \n 2. _100 \n 3. $myMoney \n 4. x \n 5. $myMoney "
+// console.log(lone)
+
+// var lone = " 1. 9 \n 2. 2student \n 3. .student \n 4. She rhoz \n 5. sher.hoz "
+// console.log(lone)
+
+
+// Chapter 5
+
+// var a = "sum of 3 and 5 is"
+// var b = (3 + 5);
+// document.write(a + " " + b)
+
+// var a = "sum of 3 and 5 is"
+// var b = (5 - 3);
+// document.write(a + " " + b)
+
+
+// var a = "sum of 3 and 5 is"
+// var b = (3 * 5);
+// document.write(a + " " + b)
+
+// var a = "sum of 3 and 5 is"
+// var b = (3 / 5);
+// document.write(a + " " + b)
+
+
+// var a = "value after variable declaration is undefined </br>"
+// document.write(a)
+
+// var b = "initial value:"
+// var c = 5;
+// document.write(b + c)
+
+// var d = " </br> value after increment is:"
+// var e = ++c;
+// document.write(d + e)
+
+// var f = "</br> value after addition is:"
+// var g = e + 7
+// document.write(f + g)
+
+// var h = "</br> value after decrement is:"
+// var i = --g
+// document.write(h + i)
+
+// var j = "</br> The remainder is:"
+// var k = i % 3
+// document.write(j + k)
+
+
+// var a = "Total Cost to buy"
+// var b = 5;
+// var c = "tickets to a movies is"
+// var d = 5*600;
+// document.write(a,b,c,d+" "+"PKR")
+
+// var Table = "Table of 5 </br>"
+// var a = 5 + "x" + 1 + "=" + 1 * 5 + "</br>";
+// var b = 5 + "x" + 2 + "=" + 2 * 5 + "</br>";
+// var c = 5 + "x" + 3 + "=" + 3 * 5 + "</br>";
+// var d = 5 + "x" + 4 + "=" + 4 * 5 + "</br>";
+// var e = 5 + "x" + 5 + "=" + 5 * 5 + "</br>";
+// var f = 5 + "x" + 6 + "=" + 6 * 5 + "</br>";
+// var g = 5 + "x" + 7 + "=" + 7 * 5 + "</br>";
+// var h = 5 + "x" + 8 + "=" + 8 * 5 + "</br>";
+// var i = 5 + "x" + 9 + "=" + 9 * 5 + "</br>";
+// var j = 5 + "x" + 10 + "=" + 10 * 5 + "</br>";
+// document.write(Table,a, b, c, d, e, f, g, h, i, j)
+
+
+// function cToF(celsius){
+//     var CeTemp = celsius;
+//     var celToFahr = CeTemp * 9/5 +32;
+//     var mess = CeTemp+'\xB0C is' + celToFahr + '\xB0F.'
+//     console.log(mess)
+// }
+// function fToC(fahrenheit){
+//     var faTemp = fahrenheit;
+//     var faToCel = (faTemp - 32 ) * 5/9;
+//     var mess = faTemp+'\xB0F is' + faToCel + '\xB0C.'
+//     console.log(mess);
+// }
+// cToF(25);
+// fToC(70);
+
+
+// var head = "<b> Shopping Cart </b>"
+// document.write(head+"<br>")
+
+// var a = "Price of item 1 is"
+// var b = 650;
+// document.write(a, b)
+
+// var c = "Quantity of item 1 is "
+// var d = 3;
+// document.write("<br>" + c, d)
+
+// var e = "Price of item 2 is"
+// var f = 100;
+// document.write("<br>" + e, f)
+
+// var g = "Quantity of item 2 is"
+// var h = 7;
+// document.write("<br>" + g, h)
+
+// var i = "shipping Charges"
+// var j = 100;
+// document.write("<br>" + i, j)
+
+// var k = "Total cost of your order is"
+// var l = (b * d) + (f * h) + 100;
+// document.write("<br>" + k + " " + l)
+
+
+
+
+// var head = "<b> Marks Sheet </b>"
+// document.write(head + "<br>")
+// var a = "Total marks:"
+// var b = 980;
+// document.write(a + b)
+// var c = "Marks obraines:"
+// var e = 804;
+// document.write("<br>" + c + e)
+// var f = "Percentage:"
+// var g = 804 * 980 / 100;
+// document.write("<br>" + f, g)
+
+
+// var head = "<b> Currency in PKR </b>"
+// document.write(head + "<br>")
+// var a = "Total Currency in PKR:"
+// var expra =  1048+700;
+// document.write(a,expra)
+
+// var a = 5*10/2;
+// document.write(a)
+
+// var head = "<b>Age Calculator</b>"
+// document.write("<br>"+head)
+// var a = prompt("Enter Current Year")
+// var b = prompt("Enter your birth year")
+// var c = "Your age is "
+// alert(a-b)
+
+// var head = "<b>Age Calculator</b>"
+// document.write("<br>"+head)
+
+// var a = "Radius of a circle:"
+// var radius = 20;
+// document.write("<br>"+a,radius)
+
+// var b = "The circumference is:"
+// var circum = 2*3.142*20;
+// document.write("<br>"+b+circum)
+
+// var c = "The area is:"
+// var area = 3.142*40;
+// document.write("<br>"+b+circum)
+
+
+// var a = "Facourite Snack: chocolate chip"
+// document.write(a)
+// var b = "Current age:"
+// var age = 15
+// document.write("<br>" + b+ age)
+// var c = "Estimated Maximum Age:"
+// var d = 65;
+// document.write("<br>"+c+d)
+// var e = "Amount of snacks per day:"
+// var f = 3;
+// document.write("<br>"+e+f)
+// var g = "you will need "
+// document.write("<br>",g,(65-15)*3)
+// document.write(" "+"chocolate chip to last you unit the rip old age of 65")
